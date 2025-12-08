2e7664265cf4d09267a5dfce805407786acd29bf
True Buddy <control@tbcpl.co.in>
2025-12-08 13:17:30 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Watchlist/",
+  base: "/WatchList/",  // must match your repo name EXACTLY
   plugins: [react()],
-})
+});
